906f76c915fe2affe54bd7008c1d2ea13bbcac01
Ming Xuan Choy <iancmx@hotmail.com>
2021-11-24 17:10:00 -0800

    Added mongoDB connection code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const logger = require('./utils/logger')
+const mongoose = require('mongoose');
+const debug = require('debug');
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -9,5 +11,11 @@ app.use(logger);
 app.get('/', (req, res) => {
     res.send('</h1> Hello World! </h1>');
 });
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/JILY", {useNewUrlParser: true, useUnifiedTopology: true})
+	.then(() => debug("Successfully connected to JILY MongoDB"))
+	.catch((err) => debug("Error connecting to database", err));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
