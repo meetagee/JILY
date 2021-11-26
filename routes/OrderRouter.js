057bdfc341f9c063f961bb1128629d2d2063408c
Ming Xuan Choy <iancmx@hotmail.com>
2021-11-25 16:57:46 -0800

    Changed some post request to put

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const orderController = require("../controllers/OrderController");
 
-router.post("/confirm/:order_id", orderController.confirm_order);
-router.post("/ready/:order_id", orderController.mark_order_ready);
-router.post("/picked-up/:order_id", orderController.mark_order_picked_up);
+router.put("/confirm/:order_id", orderController.confirm_order);
+router.put("/ready/:order_id", orderController.mark_order_ready);
+router.put("/picked-up/:order_id", orderController.mark_order_picked_up);
 router.post("/new-order/", orderController.create_order);
 router.get("/get-orders/:merchant_id", orderController.get_orders);
 
