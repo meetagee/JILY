8112ae93a5f632623ba0015610f4472a6744c2c9
Ming Xuan Choy <iancmx@hotmail.com>
2021-11-25 16:56:26 -0800

    Fix typo

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -6,7 +6,7 @@ const orderController = require("../controllers/OrderController");
 router.post("/confirm/:order_id", orderController.confirm_order);
 router.post("/ready/:order_id", orderController.mark_order_ready);
 router.post("/picked-up/:order_id", orderController.mark_order_picked_up);
-router.post("/new_order/", orderController.create_order);
+router.post("/new-order/", orderController.create_order);
 router.get("/get-orders/:merchant_id", orderController.get_orders);
 
 module.exports = router;
